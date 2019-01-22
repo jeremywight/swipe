9d970aedb088263bc225e3270692f4f083ebc147
Jeremy Wight <jeremy@invisionapp.com>
2019-01-22 07:28:53 -0500

    Returning defaultProps to fix error

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 React Native Swipe Gesture Boostrap App
 
 This app is a start at learning advanced swiping animations (similar to Tinder) through the Advanced React Native Udemy course
+
+
+To start the app cd into the `swipe` dir and run 
+(This app leverages Expo, if you do not have expo-cli installed this will prompt you to install expo globally)
+
+`npm start`
+
+This should start the expo environment, from which you can trigger the starting of the iOS or Android Simulator. Especially for Android this app is best used on a physical device due to animation, which you can get from scanning the QR Code in the expo environment.
--- src/Deck.js
+++ src/Deck.js
@@ -11,6 +11,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 200;
 
 class Deck extends Component {
+static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+}
+
     constructor(props) {
         super(props);
 
@@ -31,7 +36,7 @@ class Deck extends Component {
                 }
         });
 
-        this.state = { panResponder, position };
+        this.state = { panResponder, position, index: 0 };
     }
 
     forceSwipe(direction) {
@@ -43,9 +48,12 @@ class Deck extends Component {
     }
 
     onSwipeComplete(direction) {
-        const { onSwipeLeft, onSwipeRight } = this.props;
+        const { onSwipeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.state.index];
 
-        direction === 'right' ? onSwipeRight() : onSwipeLeft();
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        this.state.position.setValue({ x:0, y:0 });
+        this.setState({ index: this.state.index + 1 });
     }
 
     resetPosition() {
